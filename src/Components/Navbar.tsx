c9ace2c91c76bc738ce57e993964999106a4b33f
OstrichWhite <rahimcsestudies@gmail.com>
2024-02-09 23:32:59 +0530

    Refactored Navbar.

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,49 +1,44 @@
+import classNames from "classnames";
+
+import { IoCartOutline } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
+
+const navbarClasses = classNames(
+  "bg-gray-800",
+  "flex",
+  "justify-between",
+  "p-4",
+  "items-center"
+);
+const storeNameClasses = classNames("text-lg", "font-bold", "text-white");
+const linkClasses = classNames("text-white", "hover:text-gray-400");
+const inputClasses = classNames(
+  "px-2",
+  "py-1",
+  "bg-gray-700",
+  "rounded-md",
+  "focus:outline-none",
+  "focus:ring-1",
+  "focus:ring-white"
+);
 const Navbar = () => (
-  <nav className="bg-gray-800 flex justify-between p-4 items-center">
-    <div className="text-white text-lg font-bold">Store Name</div>
+  <nav className={navbarClasses}>
+    <div className={storeNameClasses}>Store Name</div>
     <div className="flex space-x-4">
-      <div className="text-white hover:text-gray-400">Home</div>
-      <div className="text-white hover:text-gray-400">Catelog</div>
-      <div className="text-white hover:text-gray-400">Contact</div>
+      <div className={linkClasses}>Home</div>
+      <div className={linkClasses}>Catelog</div>
+      <div className={linkClasses}>Contact</div>
     </div>
-    <div className="flex items-center space-x-4">
-      <div className=" relative text-white">
-        <input
-          type="text"
-          placeholder="Search"
-          className=" px-2 py-1 bg-gray-700 rounded-md focus:outline-none focus:ring-1 focus:ring-white"
-        />
+    <div className="flex items-center">
+      <div className="flex items-center text-white">
+        <input type="text" placeholder="Search" className={inputClasses} />
+        <IoIosSearch className="relative right-6 size-5" />
       </div>
-      <div className="text-white hover:text-gray-400">Cart</div>
+      <div className="text-white hover:text-gray-400">
+        <IoCartOutline className="size-8" />
+      </div>
     </div>
   </nav>
 );
 
 export default Navbar;
-
-{
-  /* 
-    <nav class="bg-gray-800 p-4 flex items-center justify-between">
-  <div class="text-white text-lg font-bold">Store Name</div>
-  <div class="flex space-x-4">
-    <div class="text-white hover:text-gray-400 transition">Home</div>
-    <div class="text-white hover:text-gray-400 transition">Catalog</div>
-    <div class="text-white hover:text-gray-400 transition">Contact</div>
-  </div>
-  <div class="text-white">Search</div>
-  <div class="relative text-white">
-  <input
-    type="text"
-    placeholder="Search..."
-    class="py-2 px-4 bg-gray-700 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-  />
-  <button class="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-white focus:outline-none">
-    <i class="fas fa-search"></i>
-  </button>
-</div>
-
-  <div class="text-white">Cart</div>
-</nav>
-
-  */
-}
