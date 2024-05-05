0a44d87ce14b11faae0a259840caa1f7bf70e853
OstrichWhite <rahimcsestudies@gmail.com>
2024-05-05 16:03:32 +0530

    Theme colors added.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: {
+          primaryBg: "#FFFFFF",
+          secondaryBg: "#F7B2B6",
+          primaryText: "#000000",
+          accent: "#FF3B30",
+        },
+        dark: {
+          primaryBg: "#121212",
+          secondaryBg: "#1E1E1E",
+          primaryText: "#FFFFFF",
+          accent: "#FF3B30",
+        },
+      },
+    },
   },
   plugins: [],
 };
